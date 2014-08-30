fde86175f0d19b73b173636761d5dcb5f3747489
Thomas Cantonnet <thomas.cantonnet@geomatic.ch>
2014-08-30 18:33:30 +0200

    Initial commit

---
+++ Module.php
@@ -0,0 +1,46 @@
+<?php
+namespace SwissEngine\Tools\Doctrine\Extension;
+
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputOption;
+use Zend\EventManager\Event;
+use Zend\ModuleManager\ModuleManagerInterface;
+
+class Module
+{
+    public function init(ModuleManagerInterface $manager)
+    {
+        // Make sure this is executed last
+        $events = $manager->getEventManager();
+        $events->getSharedManager()->attach('doctrine', 'loadCli.post', [
+            $this, 'setEntityManager'
+        ], -1000);
+    }
+
+    public function setEntityManager(Event $e) {
+        /* @var $cli \Symfony\Component\Console\Application */
+        $cli = $e->getTarget();
+
+        // Set new option for all commands
+        $commands = $cli->all();
+
+        foreach ($commands as $command) {
+            $command->getDefinition()->addOption(new InputOption('em', null, InputOption::VALUE_OPTIONAL, 'Set the name of your entity manager (overrides orm_default)'));
+        }
+
+        // Get command-line arguments
+        $args = new ArgvInput();
+        $em   = $args->getParameterOption('--em');
+
+        // Set the new em if needed
+        if ($em) {
+            /* @var $serviceLocator \Zend\ServiceManager\ServiceLocatorInterface */
+            $serviceLocator = $e->getParam('ServiceManager');
+
+            /* @var $entityManager \Doctrine\ORM\EntityManager */
+            $entityManager = $serviceLocator->get(sprintf('doctrine.entitymanager.%s', $em));
+            $helperSet     = $cli->getHelperSet();
+            $helperSet->set(new EntityManagerHelper($entityManager), 'em');
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+SwissEngine\ErrorHandler
+===============
+This small module aims at providing a simple way to add a few features (seamlessly) to the doctrine CLI tool (ZF2) such as the ability to specify the entity manager we want to use optionally.
+
+`php public/index.php orm:validate-schema --em=orm_custom`
+
+Installation
+------------
+
+Suggested installation method is through [composer](http://getcomposer.org/):
+
+```php
+php composer.phar require swissengine/doctrine-module-extension:dev-master
+```
+
+Setup
+-----
+
+If you use Zend Framework 2, you can now enable this module in your application by adding it to `config/application.config.php` as `SwissEngine\Tools\Doctrine\Extension`.
